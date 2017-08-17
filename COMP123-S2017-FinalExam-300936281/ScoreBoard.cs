094143987617f2e70b625cd31d18bb5837a7c427
aldrin312 <aldrin_fernandez312@yahoo.com>
2017-08-17 13:08:04 -0400

    creater the splashform and scoreboard class and its intances and properties

--- COMP123-S2017-FinalExam-300936281/ScoreBoard.cs
+++ COMP123-S2017-FinalExam-300936281/ScoreBoard.cs
@@ -9,8 +9,77 @@ namespace COMP123_S2017_FinalExam_300936281
 {
     class ScoreBoard
     {
+        //private instances
         private TextBox _finalScoreTextBox;
         private int _score;
-        private TextBox 
+        private TextBox _scoreTextBox;
+        private int _timer;
+        private TextBox _timerTextbox;
+
+        //publuc properties
+
+        public TextBox FinalScoreTextBox
+        {
+            get
+            {
+                return this._finalScoreTextBox;
+            }
+            set
+            {
+                this._finalScoreTextBox = value;
+
+            }
+        }
+        public int Score
+        {
+            get
+            {
+                return this._score;
+            }
+            set
+            {
+                this._score = value;
+
+            }
+        }
+
+        public TextBox ScoreTextBox
+        {
+            get
+            {
+                return this._scoreTextBox;
+            }
+            set
+            {
+                this._scoreTextBox = value;
+
+            }
+        }
+        public int Timer
+        {
+            get
+            {
+                return this._timer;
+            }
+            set
+            {
+                this._timer = value;
+
+            }
+        }
+        public TextBox TimerTextbox
+        {
+            get
+            {
+                return this._timerTextbox;
+            }
+            set
+            {
+                this._timerTextbox = value;
+
+            }
+        }
+
+
     }
 }
